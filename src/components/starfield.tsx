d7f7c978f7f0f92a960ca6083a795c58f7f9765c
Vinish V <vinish458@gmail.com>
2025-07-27 06:38:35 +0000

    remove the constellation lines

--- src/components/starfield.tsx
+++ src/components/starfield.tsx
@@ -18,49 +18,35 @@ const Starfield: React.FC = () => {
         let stars: Star[] = [];
         const numStars = 500;
         
-        let mouse = {
-            x: width / 2,
-            y: height / 2,
-            isSet: false
-        };
-
         function random(min: number, max: number) {
             return Math.random() * (max - min) + min;
-        }
-        
-        function getStarScreenCoords(star: Star) {
-            const sx = (star.x / star.z) * width/2 + width/2;
-            const sy = (star.y / star.z) * height/2 + height/2;
-            return { sx, sy };
         }
 
         class Star {
             x: number;
             y: number;
             z: number;
-            pz: number;
 
             constructor() {
-                this.x = random(-width, width);
-                this.y = random(-height, height);
+                this.x = random(-width / 2, width / 2);
+                this.y = random(-height / 2, height / 2);
                 this.z = random(0, width);
-                this.pz = this.z;
             }
 
             update() {
                 this.z -= 2; // speed
                 if (this.z < 1) {
                     this.z = width;
-                    this.x = random(-width, width);
-                    this.y = random(-height, height);
-                    this.pz = this.z;
+                    this.x = random(-width / 2, width / 2);
+                    this.y = random(-height / 2, height / 2);
                 }
             }
 
             draw() {
                 ctx!.fillStyle = 'rgba(255, 255, 255, 0.8)';
                 
-                const { sx, sy } = getStarScreenCoords(this);
+                const sx = this.x * (width / this.z) + width / 2;
+                const sy = this.y * (height / this.z) + height / 2;
 
                 const r = Math.max(0.1, 2.5 * (1 - this.z / width));
 
@@ -70,25 +56,6 @@ const Starfield: React.FC = () => {
             }
         }
         
-        const handleMouseMove = (e: MouseEvent) => {
-            mouse.x = e.clientX;
-            mouse.y = e.clientY;
-            mouse.isSet = true;
-        };
-        
-        const handleTouchMove = (e: TouchEvent) => {
-          if (e.touches.length > 0) {
-            mouse.x = e.touches[0].clientX;
-            mouse.y = e.touches[0].clientY;
-            mouse.isSet = true;
-          }
-        }
-
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('touchmove', handleTouchMove);
-        window.addEventListener('touchstart', (e) => handleTouchMove(e as unknown as TouchEvent));
-
-
         for (let i = 0; i < numStars; i++) {
             stars.push(new Star());
         }
@@ -102,35 +69,6 @@ const Starfield: React.FC = () => {
                 star.update();
                 star.draw();
             });
-            
-            // Draw constellation lines
-            if(mouse.isSet){
-                ctx!.strokeStyle = 'rgba(255, 255, 255, 0.1)';
-                for (let i = 0; i < stars.length; i++) {
-                    const star1 = stars[i];
-                    const { sx: x1, sy: y1 } = getStarScreenCoords(star1);
-                    
-                    // Check distance from mouse
-                    const distToMouse = Math.hypot(x1 - mouse.x, y1 - mouse.y);
-
-                    if (distToMouse < 150) { // Only connect stars near the cursor
-                         for (let j = i + 1; j < stars.length; j++) {
-                            const star2 = stars[j];
-                            const { sx: x2, sy: y2 } = getStarScreenCoords(star2);
-                            
-                            const dist = Math.hypot(x1 - x2, y1 - y2);
-
-                            if (dist < 100) { // Max distance between stars to connect
-                                ctx!.beginPath();
-                                ctx!.moveTo(x1, y1);
-                                ctx!.lineTo(x2, y2);
-                                ctx!.stroke();
-                            }
-                        }
-                    }
-                }
-            }
-
 
             animationFrameId = requestAnimationFrame(loop);
         }
@@ -150,9 +88,6 @@ const Starfield: React.FC = () => {
 
         return () => {
             window.removeEventListener('resize', handleResize);
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('touchmove', handleTouchMove);
-            window.removeEventListener('touchstart', (e) => handleTouchMove(e as unknown as TouchEvent));
             cancelAnimationFrame(animationFrameId);
         };
     }, []);
