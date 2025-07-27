ca2216bb03d3e540ead4db6d40e3224c209891eb
Vinish V <vinish458@gmail.com>
2025-07-27 06:16:21 +0000

    add fire works to the entire page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from 'next/font/google';
+import Fireworks from '@/components/fireworks';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${inter.variable} font-body antialiased`}>
+        <Fireworks />
         {children}
         <Toaster />
       </body>
